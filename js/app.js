01429a4250efa556a4c5637c830dd973c110e8a5
Marlon Surban <marlon.surban@aptiture.com>
2018-09-23 12:43:31 +0800

    simplified code and changed algorithm

--- js/app.js
+++ js/app.js
@@ -1,47 +1,75 @@
-window.onload = function () {
-	var winning_digits = [];
-	var bet_digits = [];
-	var interval_time = 500; // time to generate next winning numbers in milliseconds
+// window.onload = function () {
+  // var bet_digits = [3, 4, 5];
+  let bet_digits = []; 
+  const interval_time = 300; // time to generate next winning numbers in milliseconds
+  let isRumble = false;
 
-	var bet_container_id = ["first-bet", "second-bet", 'third-bet'];
-	var winning_container_id = ["first-winning", "second-winning", 'third-winning'];
+	const bet_container_id = [ 'first-bet', 'second-bet', 'third-bet'];
+	const winning_container_id = ['first-winning', 'second-winning', 'third-winning'];
 
-	init();
+	function init() {
+    getBetDigits(bet_container_id);
+    let draws = 0;
 
-	function init () {		
-		// var draw_lottery = setInterval(function () {
+		var draw_lottery = setInterval(function () {
+      console.log('bet----: ' + bet_digits);
+      let winning_digits = [];
+      let didWin = false;
+      // draw first
+      winning_digits = generateWinningNumbers();
+      // get winning numbers 
+      console.log('winning: '+ winning_digits);
+      didWin = isArrayEqual(bet_digits, winning_digits, true);
+      console.log(isArrayEqual(bet_digits, winning_digits));
+      console.log('draws: ' + draws++);
+      console.log('-------------------------');
 
-			if (checkWinner()) {
-				clearInterval(draw_lottery);
-				alert("you won!")
-			}
-			else {
-				bet_digits = [];
-				drawNumbers();
-			}
-		// }, interval_time);
+      if (didWin) {
+        alert('you won');
+        clearInterval(draw_lottery);
+      } else {
+        return; 
+      }
+		}, interval_time);
+  }
+
+  function isArrayEqual(arr1, arr2, rumble) {
+    let firstArray = [], secondArray = [];
+    if (rumble) {
+      firstArray = arr1.sort();
+      secondArray = arr2.sort();
+    } else {
+      firstArray = arr1;
+      secondArray = arr2;
+    }
+    for (let index = 0; index < secondArray.length; index++) {
+      if(firstArray[index] !== secondArray[index]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function getBetDigits(bet_container_id) {
+    bet_container_id.map( elementId => {
+      var bet = document.getElementById(elementId).value;
+      bet_digits.push(parseInt(bet));
+    });
+  }
+
+	function generateWinningNumbers() {
+    var winning_number = [];
+    
+    while (winning_number.length < 3) {
+      winning_number.push(pickRandomNumber());
+    }
+
+    return winning_number; 
 	}
 
-	function checkWinner () {
-		 return false;
-	}
-
-	function drawNumbers () {
-		winning_container_id.forEach( function(element_id) {
-			generateWinningNumbers(element_id);
-		});
-	}
-
-	function generateWinningNumbers (element_id) {
-		var winning_number = pickRandomNumber();
-		winning_digits.push(winning_number);
-		var number_node = document.createTextNode(winning_number);
-		document.getElementById(element_id).appendChild(number_node);
-	}
-
-	function pickRandomNumber () {
+	function pickRandomNumber() {
 		 var random_zero_to_nine = parseInt(Math.floor((Math.random() * 10)));
 		 return random_zero_to_nine;
 	}
 
-}
+// }
